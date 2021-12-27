80efb8821caf540d99b537e2634a38cfc97c318c
IBXCODECAT <nathan.schmitt7@gmail.com>
2021-12-27 17:52:26 -0600

    Update Compiler

--- src/boot.asm
+++ src/boot.asm
@@ -1,12 +1,19 @@
-org 0x7c00
+[org 0x7c00]
 
-mov [BOOT_DRIVE], dl ;Store disk number program was loaded from into register dl
+mov [BOOT_DISK], dl ;Store disk number program was loaded from into register dl
 
 mov bp, 0x7c00
 mov sp, bp
 
-%include "../src/dskread.asm" ;Include is relative to the builder.bat file location...
 call ReadDisk ;Call readDisk from dskread.asm
 
+mov ah, 0x0e
+mov al, [PROGRAM_SPACE]
+int 0x10
+
+jmp $
+
+%include "../src/dskread.asm" ;Include path is relative to the builder.bat file location...
+
 times 0200h - 2 - ($ - $$)  db 0 ;Zerofill up to 510 bytes
-	dw 0AA55h ;Boot Sector signature
+dw 0AA55h ;Boot Sector signature
--- src/dskread.asm
+++ src/dskread.asm
@@ -1,22 +1,25 @@
-
 PROGRAM_SPACE equ 0x7e00 ;Offset which shoves everything after boot.asm binary output
 
-BOOT_DRIVE:
+BOOT_DISK:
 	db 0
 
 ReadDisk:
+	mov ah, 0x02 ;BIOS Function for reading a disk...
 	mov bx, PROGRAM_SPACE
-	mov al, 4 ;Read 4 disk sectors (~2000 bytes)
-	mov dl, [BOOT_DRIVE] ;Select drive
+	mov al, 2 ;Read 2 disk sectors (~2000 bytes)
+	mov dl, [BOOT_DISK] ;Select drive
 	mov ch, 0x00 ;Select drive cylander 0 (harddrive specific)
 	mov dh, 0x00 ;Select drive head 0 (harddrive specific)
-	ret cl, 0x02 ;Select disk sector 2
+	mov cl, 0x02 ;Select disk sector 2
 	
 	int 0x13 ;BIOS interupt to read disk (Sets a special register if read fails)
-	
 	jc DiskReadFailed ;Jump to DiskReadFailed if disk read interupt failed
-
+	
 	ret
 
+DiskReadError:
+	db "Disk read error",0
+
 DiskReadFailed:
+	mov bx, DiskReadError
 	jmp $
